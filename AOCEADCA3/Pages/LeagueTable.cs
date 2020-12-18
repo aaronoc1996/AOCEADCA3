7abe434d544d4978f8ab87b755cfd4398223bfc9
aaron o'connor <aaronoconnor4@gmail.com>
2020-12-18 05:41:01 +0000

    league Table

---
+++ AOCEADCA3/Pages/LeagueTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using System.Linq;
+using System.Threading.Tasks;
+using AOCEADCA3.Shared;
+
+namespace AOCEADCA3.Pages
+{
+        public partial class LeagueTable : ComponentBase
+        {
+            private Root Main;
+            private string ErrorMessage;
+            private string League = "PL";
+
+            private async Task GetDataAsync()
+            {
+                try
+                {
+                    string uri = "https://api.football-data.org/v2/competitions/" + League + "/standings?standingType=TOTAL";
+                    Main = await Http.GetJsonAsync<Root>(uri);
+                    ErrorMessage = String.Empty;
+                }
+                catch (Exception e)
+                {
+                    ErrorMessage = e.Message;
+                }
+            }
+
+
+            protected override async Task OnInitializedAsync()
+            {
+                await GetDataAsync();
+            }
+
+        }
+    }
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# AOCEADCA3
